7cd18374c972fe9a245f9bbf857d441a00866f9a
Morozov Artem <69896562+morozgit@users.noreply.github.com>
2025-04-29 16:58:59 +0300

    delete file and dir

---
+++ frontend/src/composables/useDelete.js
@@ -0,0 +1,28 @@
+import { ref } from 'vue';
+import toastr from 'toastr';
+import axios from 'axios';
+
+export const useDelete = () => {
+  const isDownloading = ref(false);
+
+  const deleteFile = async (item) => {
+    isDownloading.value = true;
+    try {
+      const payload = {
+        path: item.path,
+        name: item.name
+      };
+
+      const response = await axios.post('/api/files/delete', payload);
+      console.log(response);
+      toastr.success('Файл удалён');
+    } catch (error) {
+      console.error('Ошибка delete', error);
+      toastr.error('Ошибка при удалении файла');
+    } finally {
+      isDownloading.value = false;
+    }
+  };
+
+  return { deleteFile }; // <-- ЭТО ВАЖНО
+};
