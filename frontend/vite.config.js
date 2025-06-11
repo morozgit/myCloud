6a11fee8c57cb9dcd2e930c231477182a861219a
Morozov Artem <69896562+morozgit@users.noreply.github.com>
2025-06-11 22:24:38 +0300

    add api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/mycloud',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '/api'),
       },
